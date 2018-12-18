5549f9c3b1e39c098b4bbc48612aff91aec98d32
hans <714559515@qq.com>
2018-12-18 15:29:26 +0800

    是dev分支吗

--- src/main/java/com/itheima/User.java
+++ src/main/java/com/itheima/User.java
@@ -11,40 +11,15 @@ public class User {
     private String username;
     private Integer age;
     private String address;
-
     private String sex;
 
-    public String getSex() {
-        return sex;
-    }
-
-    public void setSex(String sex) {
-        this.sex = sex;
-    }
-
-
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", age=" + age +
+                ", address='" + address + '\'' +
+                ", sex='" + sex + '\'' +
+                '}';
     }
 }

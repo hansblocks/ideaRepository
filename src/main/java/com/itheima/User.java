8a7d4fde8e59e16491bcbee50666f86ea49069b5
hans <714559515@qq.com>
2018-12-18 15:32:24 +0800

    这是master分支下的

--- src/main/java/com/itheima/User.java
+++ src/main/java/com/itheima/User.java
@@ -8,9 +8,32 @@ package com.itheima;
 
 
 public class User {
+    /*这是master分支下的*/
     private String username;
     private Integer age;
     private String address;
 
+    public String getUsername() {
+        return username;
+    }
 
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }

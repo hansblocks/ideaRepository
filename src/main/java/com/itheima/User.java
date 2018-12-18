2e30a9f9eaeea79b7569373260f081ce38c1dfc6
hans <714559515@qq.com>
2018-12-18 11:40:33 +0800

    新增password属性

--- src/main/java/com/itheima/User.java
+++ src/main/java/com/itheima/User.java
@@ -12,6 +12,16 @@ public class User {
     private Integer age;
     private String address;
 
+    private String password;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     public String getAddress() {
         return address;
     }

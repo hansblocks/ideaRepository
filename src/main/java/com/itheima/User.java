c8dfc0b15db86cf3a6a19b78702667a907ebb19a
hans <714559515@qq.com>
2018-12-18 09:12:38 +0800

    新建User.java，提交到本地仓库

---
+++ src/main/java/com/itheima/User.java
@@ -0,0 +1,29 @@
+package com.itheima;
+/*
+ * Package_Name:com.itheima
+ * Author:PP_Hu
+ * Date:2018/12/18 0018
+ * Time:上午 9:06
+ */
+
+
+public class User {
+    private String username;
+    private Integer age;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}

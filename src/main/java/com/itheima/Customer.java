9f0530a4e3f440bbe6d3c4dfa44f033ecbe760c0
hans <714559515@qq.com>
2018-12-18 15:22:08 +0800

    更改user 新增customer

---
+++ src/main/java/com/itheima/Customer.java
@@ -0,0 +1,12 @@
+package com.itheima;
+/*
+ * Package_Name:com.itheima
+ * Author:PP_Hu
+ * Date:2018/12/18 0018
+ * Time:下午 3:20
+ */
+
+
+public class Customer {
+    String username;
+}
--- src/main/java/com/itheima/User.java
+++ src/main/java/com/itheima/User.java
@@ -12,39 +12,5 @@ public class User {
     private Integer age;
     private String address;
 
-    private String sex;
 
-    public String getSex() {
-        return sex;
-    }
-
-    public void setSex(String sex) {
-        this.sex = sex;
-    }
-
-
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
 }
